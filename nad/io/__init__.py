436b3217c82ac82ec3b54226098bf0aeb28f9d67
duuugh <25300120165@m.fudan.edu.cn>
2026-04-27 20:19:45 +0800

    Delete nad directory

--- nad/io/__init__.py
+++
@@ -1,5 +0,0 @@
-# NAD Next - IO subpackage
-# Exposes high-level cache loader and helpers for visualization tools
-from .loader import NadNextLoader, detect_nad_next_cache
-from .index import ensure_loader, load_nad_next_index
-from .viz_catalog import build_problem_catalog
--- nad/ops/__init__.py
+++
@@ -1,3 +0,0 @@
-# NAD Next - Ops subpackage
-from .accuracy import compute_accuracy_report
-from .uniques import extract_tokenwise_counts
